9ceec0d3805f91a11785387b7b231aba9b3a879e
teamconvergence <46495005+teamconvergence@users.noreply.github.com>
2019-01-14 19:22:10 -0800

    Robot can now roughly go to white tape automatically

---
+++ robot/RobotMap.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+
+	public static int p_xbox1 = 0;
+
+	// DriveTrain Motors
+	public static int p_leftDrive1 = 0;
+	// public static int p_leftDrive2 = 1;
+	public static int p_rightDrive1 = 1;
+	// public static int p_rightDrive2 = 3;
+
+}
